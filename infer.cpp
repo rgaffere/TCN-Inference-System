f61c6fb932e4f94af8e7bd555ea2f04966961c59
Ryan <121699397+rgaffere@users.noreply.github.com>
2026-04-27 12:02:58 -0400

    Update infer.cpp
    changed to 3d kernel, two dilations per block, add residual after second relu. new relu function due to temp format in memory

--- infer.cpp
+++ infer.cpp
@@ -7,6 +7,10 @@ void ReLU(double * in, int len) {
     }
 }
 
+void ReLU_one(double& x) {
+    x = (x > 0) ? x : 0;
+}
+
 double dilatedConv(double * in, double * kernel, double b, int inLen, int k, int d) {
     // in - input sequence, format : last entry is the latest entry
     // kernel - filter, format : same as the input, last weight is for latest entry
@@ -24,7 +28,7 @@ double dilatedConv(double * in, double * kernel, double b, int inLen, int k, int
     return out;
 }
 
-double * doHiddenLayer(double * in, int inLen, double * kernel, double * b, int inChannels, int k, int outChannels, int d, double * residual) {
+double * doHiddenLayer(double * in, int inLen, double * kernel1, double * kernel2, double * b1, double * b2, double * temp, int inChannels, int k, int outChannels, int d, double * residual) {
     // This function does the dilated convolution + ReLU + residual for a single hidden layer (or the output layer)
     // This function returns a 1d array of length kWidth composed of the latest outputs in a single layer
 
@@ -32,18 +36,30 @@ double * doHiddenLayer(double * in, int inLen, double * kernel, double * b, int 
     // inLen - this is the length of the input layer
     // kernel - this is the filter, can be 3d if the input sequence has multiple channels
     // b - this is an array of biases for each channel, should be 1d
+    // temp - this is an intermediate hidden layer since one resi block has two dilations
     // k - this is the length of one individual kernal
     // inChannels - this is the length of the kernel, to be passed into the dilatedConv function
     // outChannels - this is the width/height of the kernel, this is how we determine the number of input channels
     // residual - this is added to each point, can be an array
-    double* out = new double[outChannels];
     for(int i = 0; i < outChannels; i++) {
-        out[i] = b[i];
+        temp[i * inLen + (inLen - 1)] = b1[i];
         for(int j = 0; j < inChannels; j++) {
-            out[i] += dilatedConv(&in[j * inLen], &kernel[(i * inChannels + j) * k], 0, inLen, k, d);
+            temp[i * inLen + (inLen - 1)] += dilatedConv(&in[j * inLen], &kernel1[(i * inChannels + j) * k], 0, inLen, k, d);
+        }
+        ReLU_one(temp[i * inLen + (inLen - 1)]);
+    }
+    
+    // now for second dilation
+    double* out = new double[outChannels];
+    
+    for(int i = 0; i < outChannels; i++) {
+        out[i] = b2[i];
+        for(int j = 0; j < outChannels; j++) {
+            out[i] += dilatedConv(&temp[j * inLen], &kernel2[(i * outChannels + j) * k], 0, inLen, k, d);
         }
     }
     ReLU(out, outChannels);
+    
     for(int i = 0; i < outChannels; i++ ) {
         out[i] += residual[i];
     }
